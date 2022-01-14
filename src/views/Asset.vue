871e47e0f2785e43ebddceec16a6f1c6d9ac55d4
Deimantas Butėnas <itsmedmd1@gmail.com>
2022-01-15 00:57:16 +0200

    9 Creating an asset component and routing for it with id param

---
+++ src/views/Asset.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="asset">
+    <h1>asset - {{ id }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import store from "@/store/store";
+
+export default defineComponent({
+  name: "Asset",
+  props: ["id"],
+  setup(props) {
+    console.log("hello from asset ", props.id);
+    console.log("store in asset:", store.state);
+  },
+});
+</script>
