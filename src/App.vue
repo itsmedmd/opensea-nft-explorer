c0ab271aa2782baea803f0dd6a36fd33f2698591
Deimantas Butėnas <itsmedmd1@gmail.com>
2022-01-17 18:35:26 +0200

    3 Creating and styling navigation

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,86 @@
 <template>
   <div class="app">
     <div class="aside">
-      <div class="aside__nav">
-        <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link>
+      <nav class="aside__nav">
+        <ul class="aside__nav-list">
+          <li class="aside__nav-item">
+            <router-link class="aside__nav-link" to="/"> Home </router-link>
+          </li>
+          <li class="aside__nav-item">
+            <router-link class="aside__nav-link" to="/about">
+              About
+            </router-link>
+          </li>
+        </ul>
+      </nav>
+
+      <div class="aside__content">
+        <div class="aside__list-container">
+          <p class="aside__list-title">Sort by:</p>
+          <ul class="aside__list">
+            <li class="aside__list-item">
+              <button
+                class="nav-button"
+                :class="{ 'nav-button--active': currentFilter === 'count' }"
+                @click="updateFilter('count')"
+              >
+                Sales count
+              </button>
+            </li>
+            <li class="aside__list-item">
+              <button
+                class="nav-button"
+                :class="{ 'nav-button--active': currentFilter === 'price' }"
+                @click="updateFilter('price')"
+              >
+                Sale price
+              </button>
+            </li>
+            <li class="aside__list-item">
+              <button
+                class="nav-button"
+                :class="{ 'nav-button--active': currentFilter === 'date' }"
+                @click="updateFilter('date')"
+              >
+                Latest sold
+              </button>
+            </li>
+          </ul>
+        </div>
+
+        <div class="aside__list-container">
+          <p class="aside__list-title">Feeling lucky?</p>
+          <ul>
+            <li class="aside__list-item">
+              <button class="nav-button">Show a random item</button>
+            </li>
+            <li class="aside__list-item">
+              <button class="nav-button">Randomise the list</button>
+            </li>
+          </ul>
+        </div>
+
+        <div class="aside__logo-container">
+          <a href="https://opensea.io" rel="noreferrer">
+            <img
+              src="./assets/images/opensea-logo.svg"
+              alt="OpenSea logo"
+              class="aside__logo"
+            />
+          </a>
+        </div>
       </div>
-      <button @click="fetchList()">fetch</button><br />
-      <button @click="fetchSingle()">fetch single</button><br />
-      <button @click="updateFilter('count')">Sort by sales count</button><br />
-      <button @click="updateFilter('date')">Sort by latest sales</button><br />
-      <button @click="updateFilter('price')">Sort by sales price</button><br />
     </div>
     <div class="content">
+      <button @click="fetchList()">fetch</button><br />
+      <button @click="fetchSingle()">fetch single</button><br />
       <router-view />
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, ref } from "vue";
+import { computed, defineComponent, onMounted, ref } from "vue";
 import AssetType from "@/types/AssetType";
 import ListType from "@/types/ListType";
 import store from "@/store/store";
@@ -75,6 +137,10 @@ export default defineComponent({
     const updateFilter = (filter: ListType) => {
       store.updateFilter(filter);
     };
+
+    const currentFilter = computed(() => {
+      return store.state.listFilter;
+    });
 
     onMounted(() => {
       const item: AssetType = {
@@ -142,6 +208,7 @@ export default defineComponent({
 
     return {
       list,
+      currentFilter,
       fetchList,
       fetchSingle,
       updateFilter,
