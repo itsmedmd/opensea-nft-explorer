68d08101db43a8b4510902640f24d4ea959ffd6e
Deimantas Butėnas <itsmedmd1@gmail.com>
2022-01-25 18:20:51 +0200

    6 Adding data fetch on mount

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@ import { defineComponent, onMounted } from "vue";
 
 import Navigation from "@/components/Navigation.vue";
 import store from "@/store/store";
-import AssetType from "@/types/AssetType";
+import fetchList from "@/assets/scripts/fetchList";
 
 export default defineComponent({
   name: "App",
@@ -40,50 +40,12 @@ export default defineComponent({
     // };
 
     onMounted(() => {
-      const item: AssetType = {
-        id: "0x28472a58a490c5e09a238847f66a68a47cc76f0f-0",
-        name: "adidas Originals: Into the Metaverse (Phase 1)",
-        description:
-          "Into the Metaverse is a collaborative NFT project between adidas Originals and NFT pioneers gmoney, Bored Ape Yacht Club and PUNKS Comic. An NFT co-created with the OG communities, creators and doers.",
-        num_sales: 20177,
-        permalink:
-          "https://opensea.io/assets/0x28472a58a490c5e09a238847f66a68a47cc76f0f/0",
-        creator: {
-          profile_img_url:
-            "https://storage.googleapis.com/opensea-static/opensea-profile/8.png",
-          username: "adidasOriginals",
-        },
-        collection: {
-          name: "adidas Originals Into the Metaverse",
-          image_url:
-            "https://lh3.googleusercontent.com/_wXtGDZ07ZelRfyfqDQkhoTX_feD_w3wdFuJ25z2ZLA1R6GFzuQ2jOGURRS8d7UEJGJd1svd61tZnGRS4zN-VXTUUVmg8Wymt-Uk=s120",
-        },
-        image_preview_url:
-          "https://lh3.googleusercontent.com/1rPdFe3vblA6-O-7Id-1D_J9nqlr5RyY1hJyFhOlKn5JbVwQcaB6IHIZlafK-VGbioeK63WonQ0n_B0R5QRPcsgU_xdOAL-d-wKo=s250",
-        animation_url:
-          "https://storage.opensea.io/files/3565db33a856b19f48396062e59e6d62.mp4",
-        image_original_url:
-          "https://ipfs.io/ipfs/Qmb4VB12RsXW6DaKranEdgnMUTzfyVBEb5eZ1v7JCEUxL1/",
-        image_url:
-          "https://lh3.googleusercontent.com/1rPdFe3vblA6-O-7Id-1D_J9nqlr5RyY1hJyFhOlKn5JbVwQcaB6IHIZlafK-VGbioeK63WonQ0n_B0R5QRPcsgU_xdOAL-d-wKo",
-        traits: [
-          {
-            trait_type: "Phase",
-            value: "1",
-          },
-        ],
-        asset_contract: {
-          address: "0x28472a58a490c5e09a238847f66a68a47cc76f0f",
-        },
-        token_id: "0",
-      };
+      console.log("App mounted, appending data");
 
-      for (let i = 0; i < 50; i++) {
-        //list.value.push(item);
+      // if there is no data for the defai;t filter - fetch it
+      if (store.state.pageCount === 0) {
+        fetchList(store.state.listFilter);
       }
-
-      //console.log("App mounted, appending data");
-      //store.appendData(list.value, "sale_count");
     });
   },
 });
